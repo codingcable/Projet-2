cc419c93c919305164eac7ccd78cf085acbba8dc
jean <coding.cable@gmail.com>
2021-04-24 06:59:49 +0200

    ajout de la note  du livre aux informations demandées

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 # fonction qui isole et extrait le nombre dans l'information affichant le nombre d'exemplaires disponibles
+
 def extraire_nombre(expression_brute):
     i = -12
     nombre_a_extraire = ''
@@ -11,31 +13,29 @@ def extraire_nombre(expression_brute):
     return int("".join(reversed(nombre_a_extraire)))
 
 
+#fonction qui converse la note en lettre en nombre
+
+def convertir_en_nombre(nombre_en_lettre):
+    notes = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five']
+    i = 0
+    while notes[i] != nombre_en_lettre:
+        i += 1
+    return i
+
+
 
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-# url = 'http://books.toscrape.com/catalogue/category/books/poetry_23/index.html'
 
 response = requests.get(url)
-
 
 if response.ok:
     soup = BeautifulSoup(response.content, 'lxml')
     titre = soup.find('h1')
     product_information_description = soup.select('div + p')
-    product_information_rating = soup.select('p.star-rating')
-    product_information_values = soup.find_all('td')
     link = soup.find_all('a')
-
-
-
-
-# #fonction qui calcule la note d'un livre
-# i = 0
-# notes = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five']
-#
-# while soup.find('p', attrs={"class": notes[i]}) != None:
-#     i += 1
-# print(soup.find('p', attrs={"class": notes[i-1]}))
+    product_information_values = soup.find_all('td')
+    product_information_rating = soup.select('p.star-rating')
+    star_rating = soup.find('p', class_ = 'star-rating')['class'][1]
 
 
 
@@ -50,6 +50,7 @@ informations_demandees["UPC"] = product_information_values[0].text
 informations_demandees["prix hors taxes"] = product_information_values[2].text
 informations_demandees["prix taxes incluses"] = product_information_values[3].text
 informations_demandees["nombres disponibles"] = extraire_nombre(product_information_values[5].text)
+informations_demandees["note"] = convertir_en_nombre(star_rating)
 
 for key in informations_demandees:
     print(key + ', ' + str(informations_demandees[key]))
